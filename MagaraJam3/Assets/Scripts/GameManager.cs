70b35c9b5c635144c0a752fd4de2ce9a39e901eb
Selim Enes Erdogan <sellim81@gmail.com>
2021-07-11 14:09:33 +0300

    Sorular Güncellendi +++

--- MagaraJam3/Assets/Scripts/GameManager.cs
+++ MagaraJam3/Assets/Scripts/GameManager.cs
@@ -132,6 +132,366 @@ public class GameManager : MonoBehaviour
             //StartCoroutine(eventsistem.Soru3Event());
             SoruEkle();
         }
+        if (suankisoru == eventler.Sorular[3])
+        {
+            if (deger == cevap)
+            {
+                //eventsistem.sonuc = "Ýzin ver";
+                MutluluguYukselt(10);
+                DunyaSaglýgýnýDusur(15);
+                ParaYükselt(20);
+            }
+            else
+            {
+                // eventsistem.sonuc = "Ýzin verme";
+                MutluluguDusur(10);
+            }
+            //StartCoroutine(eventsistem.Soru3Event());
+            SoruEkle();
+        }
+        if (suankisoru == eventler.Sorular[4])
+        {
+            if (deger == cevap)
+            {
+                //eventsistem.sonuc = "Onayla";
+                MutluluguDusur(10);
+                DunyaSaglýgýnýYükselt(10);
+                ParaDusur(10);
+            }
+            else
+            {
+                // eventsistem.sonuc = "Onaylama";
+                MutluluguYukselt(10);
+                DunyaSaglýgýnýDusur(10);
+                ParaYükselt(10);
+            }
+            //StartCoroutine(eventsistem.Soru3Event());
+            SoruEkle();
+        }
+        if (suankisoru == eventler.Sorular[5])
+        {
+            if (deger == cevap)
+            {
+                //eventsistem.sonuc = "Maaþlarý yükselt";
+                MutluluguYukselt(10);
+                DunyaSaglýgýnýYükselt(10);
+                ParaDusur(10);
+            }
+            else
+            {
+                // eventsistem.sonuc = "Maaþlar ayný kalsýn";
+                MutluluguDusur(10);
+                DunyaSaglýgýnýDusur(10);
+                ParaYükselt(10);
+            }
+            //StartCoroutine(eventsistem.Soru3Event());
+            SoruEkle();
+        }
+        if (suankisoru == eventler.Sorular[6])
+        {
+            if (deger == cevap)
+            {
+                //eventsistem.sonuc = "Ýnekleri uyut";
+                MutluluguDusur(10);
+                DunyaSaglýgýnýYükselt(10);
+                ParaDusur(10);
+            }
+            else
+            {
+                // eventsistem.sonuc = "Uyutma";
+                MutluluguYukselt(10);
+                DunyaSaglýgýnýDusur(10);
+            }
+            //StartCoroutine(eventsistem.Soru3Event());
+            SoruEkle();
+        }
+        if (suankisoru == eventler.Sorular[7])
+        {
+            if (deger == cevap)
+            {
+                //eventsistem.sonuc = "Petrol çýkartma";
+                MutluluguYukselt(10);
+            }
+            else
+            {
+                // eventsistem.sonuc = "Petrol çýkart";
+                MutluluguDusur(10);
+                DunyaSaglýgýnýDusur(10);
+                ParaYükselt(10);
+            }
+            //StartCoroutine(eventsistem.Soru3Event());
+            SoruEkle();
+        }
+        if (suankisoru == eventler.Sorular[8])
+        {
+            if (deger == cevap)
+            {
+                //eventsistem.sonuc = "Denizleri temizle";
+                MutluluguYukselt(10);
+                DunyaSaglýgýnýYükselt(10);
+                ParaDusur(10);
+            }
+            else
+            {
+                // eventsistem.sonuc = "Dokunma";
+                MutluluguDusur(10);
+                DunyaSaglýgýnýDusur(10);
+            }
+            //StartCoroutine(eventsistem.Soru3Event());
+            SoruEkle();
+        }
+        if (suankisoru == eventler.Sorular[9])
+        {
+            if (deger == cevap)
+            {
+                //eventsistem.sonuc = "Fabrika imarýna karþý çýk";
+                MutluluguYukselt(10);
+                ParaDusur(15);
+            }
+            else
+            {
+                // eventsistem.sonuc = "Göz yum";
+                MutluluguDusur(10);
+                DunyaSaglýgýnýDusur(10);
+                ParaYükselt(15);
+            }
+            //StartCoroutine(eventsistem.Soru3Event());
+            SoruEkle();
+        }
+        if (suankisoru == eventler.Sorular[10])
+        {
+            if (deger == cevap)
+            {
+                //eventsistem.sonuc = "Kanser aþýsý çip taktýr";
+                MutluluguDusur(15);
+                DunyaSaglýgýnýYükselt(5);
+                ParaYükselt(15);
+            }
+            else
+            {
+                // eventsistem.sonuc = "Çip taktýrma";
+                MutluluguYukselt(15);
+                DunyaSaglýgýnýDusur(5);
+                ParaDusur(5);
+            }
+            //StartCoroutine(eventsistem.Soru3Event());
+            SoruEkle();
+        }
+        if (suankisoru == eventler.Sorular[11])
+        {
+            if (deger == cevap)
+            {
+                //eventsistem.sonuc = "BikBok yasakla";
+                MutluluguDusur(10);
+                DunyaSaglýgýnýYükselt(10);
+                ParaDusur(10);
+            }
+            else
+            {
+                // eventsistem.sonuc = "Yasaklama";
+                DunyaSaglýgýnýDusur(10);
+                ParaYükselt(10);
+                MutluluguYukselt(10);
+            }
+            //StartCoroutine(eventsistem.Soru3Event());
+            SoruEkle();
+        }
+        if (suankisoru == eventler.Sorular[12])
+        {
+            if (deger == cevap)
+            {
+                //eventsistem.sonuc = "Yýkýcý deprem maðdurlarýna yardým et";
+                DunyaSaglýgýnýYükselt(10);
+                ParaDusur(20);
+                MutluluguYukselt(10);
+            }
+            else
+            {
+                // eventsistem.sonuc = "Yardým etme";
+                DunyaSaglýgýnýDusur(10);
+                MutluluguDusur(15);
+            }
+            //StartCoroutine(eventsistem.Soru3Event());
+            SoruEkle();
+        }
+        if (suankisoru == eventler.Sorular[13])
+        {
+            if (deger == cevap)
+            {
+                //eventsistem.sonuc = "Karþý çýk";
+                MutluluguYukselt(10);
+                DunyaSaglýgýnýYükselt(10);
+                ParaDusur(10);
+            }
+            else
+            {
+                // eventsistem.sonuc = "Terör örgütüne silah gönder";
+                DunyaSaglýgýnýDusur(10);
+                ParaYükselt(20);
+                MutluluguDusur(10);
+            }
+            //StartCoroutine(eventsistem.Soru3Event());
+            SoruEkle();
+        }
+        if (suankisoru == eventler.Sorular[14])
+        {
+            if (deger == cevap)
+            {
+                //eventsistem.sonuc = "Afrikada suya yardým et";
+                MutluluguYukselt(10);
+                DunyaSaglýgýnýYükselt(10);
+                ParaDusur(10);
+            }
+            else
+            {
+                // eventsistem.sonuc = "Yardým etme";
+                DunyaSaglýgýnýDusur(10);
+                MutluluguDusur(10);
+            }
+            //StartCoroutine(eventsistem.Soru3Event());
+            SoruEkle();
+        }
+        if (suankisoru == eventler.Sorular[15])
+        {
+            if (deger == cevap)
+            {
+                //eventsistem.sonuc = "Tsunami maðdurlarýna yardým et";
+                MutluluguYukselt(10);
+                DunyaSaglýgýnýYükselt(10);
+                ParaDusur(10);
+            }
+            else
+            {
+                // eventsistem.sonuc = "Yardým etme";
+                DunyaSaglýgýnýDusur(10);
+                MutluluguDusur(10);
+            }
+            //StartCoroutine(eventsistem.Soru3Event());
+            SoruEkle();
+        }
+        if (suankisoru == eventler.Sorular[16])
+        {
+            if (deger == cevap)
+            {
+                //eventsistem.sonuc = "Yeni konsollara vergi yapma";
+                MutluluguYukselt(10);
+                ParaDusur(5);
+            }
+            else
+            {
+                // eventsistem.sonuc = "Yeni oyun konsollarýný vergilendir";
+                ParaYükselt(20);
+                MutluluguDusur(10);
+            }
+            //StartCoroutine(eventsistem.Soru3Event());
+            SoruEkle();
+        }
+        if (suankisoru == eventler.Sorular[17])
+        {
+            if (deger == cevap)
+            {
+                //eventsistem.sonuc = "Gemi ticareti yasaklansýn";
+                MutluluguDusur(5);
+                DunyaSaglýgýnýYükselt(10);
+                ParaDusur(20);
+            }
+            else
+            {
+                // eventsistem.sonuc = "Yasaklanmasýn";
+                DunyaSaglýgýnýDusur(10);
+                ParaYükselt(10);
+                MutluluguYukselt(5);
+            }
+            //StartCoroutine(eventsistem.Soru3Event());
+            SoruEkle();
+        }
+        if (suankisoru == eventler.Sorular[18])
+        {
+            if (deger == cevap)
+            {
+                //eventsistem.sonuc = "Rehinleri kurtr";
+                MutluluguYukselt(20);
+            }
+            else
+            {
+                // eventsistem.sonuc = "Oðlunu kurtar";
+                MutluluguDusur(20);
+            }
+            //StartCoroutine(eventsistem.Soru3Event());
+            SoruEkle();
+        }
+        if (suankisoru == eventler.Sorular[19])
+        {
+            if (deger == cevap)
+            {
+                //eventsistem.sonuc = "Teöristlere para ver";
+                MutluluguYukselt(10);
+                ParaDusur(20);
+            }
+            else
+            {
+                // eventsistem.sonuc = "Verme";
+                MutluluguDusur(20);
+            }
+            //StartCoroutine(eventsistem.Soru3Event());
+            SoruEkle();
+        }
+        if (suankisoru == eventler.Sorular[20])
+        {
+            if (deger == cevap)
+            {
+                //eventsistem.sonuc = "Fosil yakýt durdurulsun";
+                MutluluguYukselt(10);
+                DunyaSaglýgýnýYükselt(10);
+                ParaDusur(10);
+            }
+            else
+            {
+                // eventsistem.sonuc = "Durdurulmasýn";
+                DunyaSaglýgýnýDusur(10);
+                ParaYükselt(10);
+                MutluluguDusur(10);
+            }
+            //StartCoroutine(eventsistem.Soru3Event());
+            SoruEkle();
+        }
+        if (suankisoru == eventler.Sorular[21])
+        {
+            if (deger == cevap)
+            {
+                //eventsistem.sonuc = "Sigaralar yasaklansýn";
+                MutluluguDusur(10);
+                DunyaSaglýgýnýYükselt(10);
+                ParaDusur(10);
+            }
+            else
+            {
+                // eventsistem.sonuc = "Yasaklanmasýn";
+                DunyaSaglýgýnýDusur(10);
+                ParaYükselt(10);
+                MutluluguYukselt(10);
+            }
+            //StartCoroutine(eventsistem.Soru3Event());
+            SoruEkle();
+        }
+        if (suankisoru == eventler.Sorular[22])
+        {
+            if (deger == cevap)
+            {
+                //eventsistem.sonuc = "Baz istasyonlarý kaldýrýlsýn";
+                DunyaSaglýgýnýYükselt(20);
+                ParaDusur(10);
+            }
+            else
+            {
+                // eventsistem.sonuc = "Kaldýrýlmasýn";
+                DunyaSaglýgýnýDusur(10);
+                ParaYükselt(10);
+            }
+            //StartCoroutine(eventsistem.Soru3Event());
+            SoruEkle();
+        }
+
     }
 
     void DunyaSaglýgýnýDusur(int dusur)
